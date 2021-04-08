179164db9e53e740b3739d1fdf904d2fdaa87070
Alex Vogt <alexvogt18@gmail.com>
2021-04-07 19:57:43 -0700

    Fixes JS bug, CSS adjustment for added list,

--- app.js
+++ app.js
@@ -8,39 +8,42 @@ alert('Welcome, ' +userName+ ' , I hope you enjoy the site!')
 
 let avage = prompt('What is my age?')
 
-if (avage == '27') {
-    alert('That\'s right!')
+if (avage === '27') {
+  alert('That\'s right!')
 }
 
 else {
-    alert('Wrong! Try Again!')
+  alert('Wrong! Try Again!')
+}
 
 let avcity = prompt('What city did I grow up in?')
 
-if (avcity == 'Birmingham') {
-    alert('That\'s right!')
+if (avcity === 'Birmingham') {
+  alert('That\'s right!')
 }
 
 else {
-    alert('Wrong! Try Again!')
-
+  alert('Wrong! Try Again!')
+}
 let avbranch = prompt('What branch of the military did I serve in?')
 
-if (avbranch == 'navy') {
-    alert('That\'s right!')
+if (avbranch === 'navy') { 
+  alert('That\'s right!')
 }
 
 else {
-    alert('Wrong! Try Again!')
-
+  alert('Wrong! Try Again!')
+}
 let avfavsport = prompt('What is my favorite sport?')
 
-if (avfavsport == 'football') {
-    alert('That\'s right!')
+
+if (avfavsport === 'football') {
+  alert('That\'s right!')
 }
 
 else {
-    alert('Wrong! Try Again!')
+  alert('Wrong! Try Again!')
+}
 
 let avuabMascot = prompt('Do you know what UAB\'s mascot is? Great! Enter below!')
 
@@ -48,10 +51,10 @@ let lowercaseavuabMascot = avuabMascot.toLowerCase()
 
 // console.log(lowercaseavuabMascot, 'This is the lowercase version')
 
-if (lowercaseavuabMascot == 'blazers') {
-    alert('That\'s right! UAB Blazers! Go Blazers!')
+if (lowercaseavuabMascot === 'blazers') {
+  alert('That\'s right! UAB Blazers! Go Blazers!')
 }
 
 else {
-    alert('Wrong! Try Again!')
+  alert('Wrong! Try Again!')
 }
