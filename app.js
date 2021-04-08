cb52b6219b3c595fdd89381753852ee32f470fc7
Alex Vogt <alexvogt18@gmail.com>
2021-04-07 21:45:36 -0700

    Nearly completes number guessing game with exception of the correct answer display

--- app.js
+++ app.js
@@ -1,60 +1,94 @@
-'use strict'
+// 'use strict'
+// // let userName = prompt('Please Enter your Name!')
 
-let userName = prompt('Please Enter your Name!')
+// // console.log(userName)
 
-// console.log(userName)
+// // alert('Welcome, ' +userName+ ' , I hope you enjoy the site!')
 
-alert('Welcome, ' +userName+ ' , I hope you enjoy the site!')
+// // let avage = prompt('What is my age?')
 
-let avage = prompt('What is my age?')
+// if (avage === '27') {
+//   alert('That\'s right!')
+// }
 
-if (avage === '27') {
-  alert('That\'s right!')
-}
+// else {
+//   alert('Wrong! Try Again!')
+// }
 
-else {
-  alert('Wrong! Try Again!')
-}
+// // let avcity = prompt('What city did I grow up in?')
 
-let avcity = prompt('What city did I grow up in?')
+// if (avcity === 'Birmingham') {
+//   alert('That\'s right!')
+// }
 
-if (avcity === 'Birmingham') {
-  alert('That\'s right!')
-}
+// else {
+//   alert('Wrong! Try Again!')
+// }
+// // let avbranch = prompt('What branch of the military did I serve in?')
 
-else {
-  alert('Wrong! Try Again!')
-}
-let avbranch = prompt('What branch of the military did I serve in?')
+// if (avbranch === 'navy') { 
+//   alert('That\'s right!')
+// }
 
-if (avbranch === 'navy') { 
-  alert('That\'s right!')
-}
-
-else {
-  alert('Wrong! Try Again!')
-}
-let avfavsport = prompt('What is my favorite sport?')
+// else {
+//   alert('Wrong! Try Again!')
+// }
+// // let avfavsport = prompt('What is my favorite sport?')
 
 
-if (avfavsport === 'football') {
-  alert('That\'s right!')
-}
+// if (avfavsport === 'football') {
+//   alert('That\'s right!')
+// }
 
-else {
-  alert('Wrong! Try Again!')
-}
+// else {
+//   alert('Wrong! Try Again!')
+// }
 
-let avuabMascot = prompt('Do you know what UAB\'s mascot is? Great! Enter below!')
+// // let avuabMascot = prompt('Do you know what UAB\'s mascot is? Great! Enter below!')
 
-let lowercaseavuabMascot = avuabMascot.toLowerCase()
+// let lowercaseavuabMascot = avuabMascot.toLowerCase()
 
-// console.log(lowercaseavuabMascot, 'This is the lowercase version')
+// // console.log(lowercaseavuabMascot, 'This is the lowercase version')
 
-if (lowercaseavuabMascot === 'blazers') {
-  alert('That\'s right! UAB Blazers! Go Blazers!')
-}
+// // if (lowercaseavuabMascot === 'blazers') {
+//   alert('That\'s right! UAB Blazers! Go Blazers!')
+// }
 
-else {
-  alert('Wrong! Try Again!')
+// else {
+//   alert('Wrong! Try Again!')
+// }
+
+function  guessingGame(){
+  
+  let guessingGameYesOrNo = prompt('Do you want to play a guessing game?');
+
+  while (guessingGameYesOrNo.toLowerCase() === 'yes' || 'y' || 'yeah'){
+        
+    let correctAnswer = Math.floor(Math.random() * 100) + 1;
+        
+    let userGuess = prompt('Please enter a number 1-100');
+        
+    let numberofGuesses = 3;
+  
+    for(let i = 0; i < numberofGuesses; i++){
+            
+      let guessLeft = numberofGuesses - i;
+         
+      if(userGuess < correctAnswer){
+            
+        userGuess = prompt(guessLeft + ' Guesses left. To Low. Please enter a number 1-100');
+      }
+      else if (userGuess > correctAnswer){
+      
+        userGuess = prompt(guessLeft + ' Guesses left. To High, Please enter a number 1-100');
+      }
+      if (guessLeft === 0){
+        alert('Oh no! You ran out of guesses! The correct answer was ' +correctAnswer);
+      }
+      if (userGuess === correctAnswer){
+        alert('You got it!');
+        break;
+      }
+    }
+  }
 }
